9d1288172c4d17777bc92dc61ee9aa63aba0106a
Juansu01 <thejuank0@hotmail.com>
2021-12-01 11:15:40 -0500

    Adding function that concatenates two arrays

--- math/0x00-linear_algebra/6-howdy_partner.py
+++ math/0x00-linear_algebra/6-howdy_partner.py
@@ -1,10 +1,7 @@
 #!/usr/bin/env python3
-import numpy as np
-
 """ This module defines function that concatenates two arrays. """
 
 
 def cat_arrays(arr1, arr2):
-    ar1 = np.array(arr1)
-    ar2 = np.array(arr2)
-    return np.concatenate([ar1, ar2]).tolist()
+    """This function concatenates two arrays."""
+    return [i for i in [*arr1, *arr2]]
