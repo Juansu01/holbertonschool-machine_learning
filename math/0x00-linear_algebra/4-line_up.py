251989a7913b98fb4f1a19ee24c5e19d5dfcf262
Juansu01 <thejuank0@hotmail.com>
2021-12-01 08:34:48 -0500

    Added function without using numpy

--- math/0x00-linear_algebra/4-line_up.py
+++ math/0x00-linear_algebra/4-line_up.py
@@ -1,14 +1,12 @@
 #!/usr/bin/env python3
-import numpy as np
-
 """ This module has a function that adds two arrays. """
 
 
 def add_arrays(arr1, arr2):
-    ar1 = np.array(arr1)
-    ar2 = np.array(arr2)
+    """Checks if both arrays have the same shape and adds them element-wise."""
     new_list = []
-    if ar1.shape != ar2.shape:
-        return None
-    new_list = np.add(ar1, ar2).tolist()
-    return new_list
+    if len(arr1) == len(arr2):
+        for i, e in enumerate(arr1):
+            new_list.append(arr1[i] + arr2[i])
+        return new_list
+    return None
