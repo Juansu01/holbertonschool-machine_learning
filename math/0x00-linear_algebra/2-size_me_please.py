8875bd0a48e7c0a9092b3d26b615f3f559c80949
Juansu01 <thejuank0@hotmail.com>
2021-12-01 07:50:53 -0500

    Implementing function without using np

--- math/0x00-linear_algebra/2-size_me_please.py
+++ math/0x00-linear_algebra/2-size_me_please.py
@@ -1,15 +1,14 @@
 #!/usr/bin/env python3
-import numpy as np
 
 """ This module has a function that returns the shape of a matrix. """
 
 
 def matrix_shape(matrix):
     """ Returns the shape of a matrix. """
-    arr = np.array(matrix)
-#    shape = []
-#    shape.append(len(matrix))
-#    shape.append(len(matrix[0]))
-#    if len(matrix[0]) > 2:
-#        shape.append(len(matrix[0][0]))
-    return list(arr.shape)
+
+    shape = []
+    shape.append(len(matrix))
+    shape.append(len(matrix[0]))
+    if len(matrix[0]) > 2:
+        shape.append(len(matrix[0][0]))
+    return shape
