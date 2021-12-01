3224914a93753c1c7227c212b522f2cf6f506e9a
Juansu01 <thejuank0@hotmail.com>
2021-12-01 09:24:08 -0500

    Refactoring

--- math/0x00-linear_algebra/5-across_the_planes.py
+++ math/0x00-linear_algebra/5-across_the_planes.py
@@ -4,12 +4,13 @@
 
 def add_matrices2D(mat1, mat2):
     """This function adds two matrices element-wise"""
-    new_matrix = [[], []]
+    new_matrix = []
     if len(mat1) != len(mat2):
         return None
     if len(mat1[0]) != len(mat2[0]):
         return None
     for i, e in enumerate(mat1):
+        new_matrix.append([])
         for j, k in enumerate(mat1[i]):
             new_matrix[i].append(mat1[i][j] + mat2[i][j])
     return new_matrix
