a50a1cde67b7fe66fbe1e6135cfff63b5bd3fc61
Juansu01 <thejuank0@hotmail.com>
2021-12-02 07:00:41 -0500

    Added function that transposes a matrix

---
+++ math/0x00-linear_algebra/11-the_western_exchange.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""This module defines a function that transposes a matrix."""
+
+
+def np_transpose(matrix):
+    """Returns a transposed matrix."""
+    return matrix.transpose()
