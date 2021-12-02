1b71d285e68e9945256964e5787be715b4e2491f
Juansu01 <thejuank0@hotmail.com>
2021-12-02 08:54:14 -0500

    Flipping ifs

--- math/0x00-linear_algebra/7-gettin_cozy.py
+++ math/0x00-linear_algebra/7-gettin_cozy.py
@@ -5,7 +5,7 @@
 def cat_matrices2D(mat1, mat2, axis=0):
     """Returns a new matrix, which is the result of concatenation."""
     if axis == 1:
-        if len(mat1) != len(mat2):
+        if len(mat1[0]) != len(mat1[0]):
             return None
 #        new_matrix = []
 #        for i, e in enumerate(mat1[:]):
@@ -14,7 +14,7 @@ def cat_matrices2D(mat1, mat2, axis=0):
 #            new_matrix[i].append(e.copy()[0])
         return [row.copy() + roww.copy() for row, roww in zip(mat1, mat2)]
     if axis == 0:
-        if len(mat1[0]) != len(mat1[0]):
+        if len(mat1) != len(mat2):
             return None
 #        for i, row in enumerate(mat1):
 #            new_matrix.append(row.copy())
