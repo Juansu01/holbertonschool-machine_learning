c25c963942d632c3be6d5db032028bc3d960bba0
Juansu01 <thejuank0@hotmail.com>
2021-12-02 09:12:50 -0500

    Reverting again lmao

--- math/0x00-linear_algebra/7-gettin_cozy.py
+++ math/0x00-linear_algebra/7-gettin_cozy.py
@@ -14,6 +14,11 @@ def cat_matrices2D(mat1, mat2, axis=0):
             new_matrix[i].append(e.copy()[0])
         return new_matrix
     if axis == 0:
+        new_matrix = []
         if len(mat1[0]) != len(mat1[0]):
             return None
-        return mat1 + mat2
+        for i, row in enumerate(mat1):
+            new_matrix.append(row.copy())
+        for i, row in enumerate(mat2):
+            new_matrix.append(row.copy())
+        return new_matrix
