9200a0d1eac28f1cd436a833b0e21f38e1a5c21f
Juansu01 <thejuank0@hotmail.com>
2021-12-02 08:20:45 -0500

    Adding a new function that concatenates matrices

--- math/0x00-linear_algebra/7-gettin_cozy.py
+++ math/0x00-linear_algebra/7-gettin_cozy.py
@@ -5,6 +5,22 @@ import numpy as np
 
 
 def cat_matrices2D(mat1, mat2, axis=0):
-    a = np.array(mat1)
-    b = np.array(mat2)
-    return np.concatenate((a, b), axis).tolist()
+    """Returns a new matrix, which is the result of concatenation."""
+    if axis == 1:
+        if len(mat1) != len(mat2):
+            return None
+        new_matrix = []
+        for i, e in enumerate(mat1[:]):
+            new_matrix.append(e.copy())
+        for i, e in enumerate(mat2[:]):
+            new_matrix[i].append(e.copy()[0])
+        return new_matrix
+    if axis == 0:
+        new_matrix = []
+        if len(mat1[0]) != len(mat1[0]):
+            return None
+        for i, row in enumerate(mat1):
+            new_matrix.append(row.copy())
+        for i, row in enumerate(mat2):
+            new_matrix.append(row.copy())
+        return new_matrix
