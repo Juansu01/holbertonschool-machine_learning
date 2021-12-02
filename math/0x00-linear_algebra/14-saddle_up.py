52f2e2cca9d3e953c1eb0fc82081f1db788b9947
Juansu01 <thejuank0@hotmail.com>
2021-12-02 06:34:09 -0500

    Adding function that multiplies matrices using numpy

---
+++ math/0x00-linear_algebra/14-saddle_up.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""This module defines a function that multiplies matrices."""
+import numpy as np
+
+
+def np_matmul(mat1, mat2):
+    """Returns the product of two matrices."""
+    return np.matmul(mat1, mat2)
