905766a2f98cee4e456d8698b144134f7a814450
Juansu01 <thejuank0@hotmail.com>
2021-12-02 09:08:56 -0500

    Trying a different way

--- math/0x00-linear_algebra/7-gettin_cozy.py
+++ math/0x00-linear_algebra/7-gettin_cozy.py
@@ -7,17 +7,13 @@ def cat_matrices2D(mat1, mat2, axis=0):
     if axis == 1:
         if len(mat1) != len(mat2):
             return None
-#        new_matrix = []
-#        for i, e in enumerate(mat1[:]):
-#            new_matrix.append(e.copy())
-#        for i, e in enumerate(mat2[:]):
-#            new_matrix[i].append(e.copy()[0])
-        return [row.copy() + roww.copy() for row, roww in zip(mat1, mat2)]
+        new_matrix = []
+        for i, e in enumerate(mat1[:]):
+            new_matrix.append(e.copy())
+        for i, e in enumerate(mat2[:]):
+            new_matrix[i].append(e.copy()[0])
+        return new_matrix
     if axis == 0:
         if len(mat1[0]) != len(mat1[0]):
             return None
-#        for i, row in enumerate(mat1):
-#            new_matrix.append(row.copy())
-#        for i, row in enumerate(mat2):
-#            new_matrix.append(row.copy())
-        return [row.copy() for row in mat1] + [row.copy() for row in mat2]
+        return mat1 + mat2
