d38ec26a75086a32b9c6832456bf3397ad458fc4
Juansu01 <thejuank0@hotmail.com>
2021-12-10 08:41:17 -0500

    Handling edge cases

--- math/0x00-linear_algebra/7-gettin_cozy.py
+++ math/0x00-linear_algebra/7-gettin_cozy.py
@@ -8,17 +8,23 @@ def cat_matrices2D(mat1, mat2, axis=0):
         if len(mat1) != len(mat2):
             return None
         new_matrix = []
-        for row1, row2 in zip(mat1, mat2):
-            copy = row1.copy()
-            copy.extend(row2.copy())
-            new_matrix.append(copy)
-        return new_matrix
+        if mat1[0] and mat2[0]:
+            for row1, row2 in zip(mat1, mat2):
+                copy = row1.copy()
+                copy.extend(row2.copy())
+                new_matrix.append(copy)
+            return new_matrix
+        else:
+            return None
     if axis == 0:
         new_matrix = []
         if len(mat1[0]) != len(mat1[0]):
             return None
-        for i, row in enumerate(mat1):
-            new_matrix.append(row.copy())
-        for i, row in enumerate(mat2):
-            new_matrix.append(row.copy())
-        return new_matrix
+        if mat1[0] and mat2[0]:
+            for i, row in enumerate(mat1):
+                new_matrix.append(row.copy())
+            for i, row in enumerate(mat2):
+                new_matrix.append(row.copy())
+            return new_matrix
+        else:
+            return None
