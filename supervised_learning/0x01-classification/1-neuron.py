37a06742fd9e43d8d7495fa5526d6cde6bcaeb22
Juansu01 <thejuank0@hotmail.com>
2021-12-25 07:27:32 -0500

    Adding class with private attributes

---
+++ supervised_learning/0x01-classification/1-neuron.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+import numpy as np
+""" This module defines the Neuron class, that
+defines a single neuron that performs binary classification.
+"""
+
+
+class Neuron():
+    """
+    This class defines a single neuron performing binary
+    classification.
+    """
+    def __init__(self, nx):
+        """Init method."""
+        if type(nx) != int:
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+
+        self.__W = np.random.randn(1, nx)
+        self.__b = 0
+        self.__A = 0
+
+    @property
+    def W(self):
+        """Privatizes the W attribute"""
+        return self.__W
+
+    @property
+    def b(self):
+        """Privatizes the b attribute"""
+        return self.__b
+
+    @property
+    def A(self):
+        """Privatizes the A attribute"""
+        return self.__A
