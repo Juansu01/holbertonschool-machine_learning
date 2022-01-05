0522391f2c190fcf4a387443e10c820102e7b7bc
Juansu01 <thejuank0@hotmail.com>
2022-01-05 07:44:38 -0500

    Adding function that creates two placeholders

---
+++ supervised_learning/0x02-tensorflow/0-create_placeholders.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+import tensorflow.compat.v1 as tf
+
+
+def create_placeholders(nx, classes):
+    """Takes in the number of feature columns, and clasess
+    returns both place holders for nx and classes
+    respectively."""
+    x = tf.placeholder("float", [None, nx], name='x')
+    y = tf.placeholder("float", [None, classes], name='y')
+
+    return x, y
