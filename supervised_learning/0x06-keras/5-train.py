5f79d5146c50dd9b711a29c0dbaa9414f7f6476b
Juansu01 <thejuank0@hotmail.com>
2022-01-25 09:48:46 -0500

    Adding a function that takes into account validation data

---
+++ supervised_learning/0x06-keras/5-train.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""This module defines a function that trains a model and
+returns the history."""
+
+
+import tensorflow.keras as K
+
+
+def train_model(network, data, labels, batch_size,
+                epochs, validation_data=None,
+                verbose=True, shuffle=False):
+    """Takes in the model, with data, labels, batch size,
+    epochs, verbose, shuffle to train the model using
+    mini-batch gradient descent. """
+
+    return network.fit(x=data, y=labels,
+                       batch_size=batch_size,
+                       epochs=epochs, verbose=verbose,
+                       validation_data=validation_data,
+                       shuffle=shuffle)
