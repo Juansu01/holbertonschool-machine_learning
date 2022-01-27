be2e72a3c67c9d05ed92b4df6056f42ff24bcac9
Juansu01 <thejuank0@hotmail.com>
2022-01-27 13:47:21 -0500

    Adding function that tests a model

---
+++ supervised_learning/0x06-keras/12-test.py
@@ -0,0 +1,16 @@
+"""
+This module defines a function that evaluates a model.
+"""
+
+
+import tensorflow.keras as K
+
+
+def test_model(network, data, labels, verbose=True):
+    """This method tests a neural network and
+    returns its loss and accuracy respectively."""
+
+    loss, acc = network.evaluate(x=data, y=labels,
+                                 verbose=verbose)
+
+    return loss, acc
