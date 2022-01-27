22dfd447c6cb3589fd7c8b579fce24e15bf187b4
Juansu01 <thejuank0@hotmail.com>
2022-01-27 13:52:34 -0500

    Testing

--- supervised_learning/0x06-keras/12-test.py
+++ supervised_learning/0x06-keras/12-test.py
@@ -11,7 +11,8 @@ def test_model(network, data, labels, verbose=True):
     """This method tests a neural network and
     returns its loss and accuracy respectively."""
 
-    loss, acc = network.evaluate(x=data, y=labels,
-                                 verbose=verbose)
+    l, a = network.evaluate(x=data,
+                            y=labels,
+                            verbose=verbose)
 
-    return loss, acc
+    return l, a
