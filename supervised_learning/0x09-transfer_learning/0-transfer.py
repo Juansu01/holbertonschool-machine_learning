dfffd9a3c89bc9d62adb2367949621c0e7ce8757
Juansu01 <thejuank0@hotmail.com>
2022-02-14 13:43:02 -0500

    Adding task file with only documentation

---
+++ supervised_learning/0x09-transfer_learning/0-transfer.py
@@ -0,0 +1,27 @@
+"""
+This script defines preprocess_data and main.
+preprocess_data - takes in input data and returns
+the preprocessed data.
+main - trains a model to classify the CIFAR 10 dataset.
+"""
+
+
+import tensorflow.keras as K
+
+
+
+def preprocess_data(X, Y):
+    """
+    X - is the input data.
+    Y - is the labels for the input data.
+    Preprocesses data for the model.
+    Returns X_p and Y_p respectively.
+    """
+    pass
+
+
+def main():
+    """
+    Trains a model, compiles it and saves it as "cifar10.h5"
+    """
+    pass
